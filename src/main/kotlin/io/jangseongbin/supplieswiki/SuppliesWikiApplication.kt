aca1f54de0ff52e5ebb7ab7f77ab06ba0e3d99ca
SheepEatLion <gowjr207@gmail.com>
2023-10-13 14:36:26 +0900

    init: springboot

---
+++ src/main/kotlin/io/jangseongbin/supplieswiki/SuppliesWikiApplication.kt
@@ -0,0 +1,11 @@
+package io.jangseongbin.supplieswiki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SuppliesWikiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SuppliesWikiApplication>(*args)
+}
---
+++ src/test/kotlin/io/jangseongbin/supplieswiki/SuppliesWikiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.jangseongbin.supplieswiki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SuppliesWikiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
