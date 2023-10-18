27019d88df3ad3b435424b3973bc911bdb34a175
SheepEatLion <gowjr207@gmail.com>
2023-10-18 18:23:45 +0900

    feat: create user domain
    유저 도메인(엔티티) 생성

---
+++ src/main/kotlin/io/jangseongbin/supplieswiki/user/domain/User.kt
@@ -0,0 +1,24 @@
+package io.jangseongbin.supplieswiki.user.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType.AUTO
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity
+class User(
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    val id: Long,
+
+    val loginId: String,
+
+    val nickname: String,
+
+    var password: String,
+
+    val createdAt: LocalDateTime,
+
+    val updatedAt: LocalDateTime,
+)
