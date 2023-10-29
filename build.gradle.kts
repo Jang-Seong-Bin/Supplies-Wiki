3570e3db5405a9eb5be75864aebcb00338e8cb4e
sshworld <59462537+sshworld@users.noreply.github.com>
2023-10-30 01:53:13 +0900

    chore: spring security, jwt dependencies 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ val snippetsDir by extra { file("build/generated-snippets") }
 val springCloudVersion by extra { "2022.0.4" }
 
 dependencies {
+	api ("com.auth0:java-jwt:4.4.0")
+	api("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
