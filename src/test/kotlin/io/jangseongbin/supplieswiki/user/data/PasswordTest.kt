8a31d05a4cb5550ba8cba98495d7f5852d0cce53
sshworld <59462537+sshworld@users.noreply.github.com>
2023-11-08 00:04:59 +0900

    feat: password test 추가

---
+++ src/test/kotlin/io/jangseongbin/supplieswiki/user/data/PasswordTest.kt
@@ -0,0 +1,26 @@
+package io.jangseongbin.supplieswiki.user.data
+
+import io.jangseongbin.supplieswiki.user.data.fixtures.password
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+class PasswordTest : ExpectSpec({
+    context("패스워드가 입력되었을 때") {
+        expect("규칙에 맞지 않는다면 IllegalStateException 을 리턴한다.") {
+            val word = "jangseongbin"
+
+            shouldThrow<IllegalStateException> {
+                password(word)
+            }
+        }
+
+        expect("규칙에 맞다면 정상적으로 생성된다.") {
+            val word = "jangseongbin123"
+            val password = password(word)
+
+            BCryptPasswordEncoder().matches("jangseongbin123", password.password) shouldBe true
+        }
+    }
+})
